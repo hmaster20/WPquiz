98a2c45a6b8dab6cb084d70429b857fa1b669c54
hmaster20 <hmaster20@gmail.com>
2025-07-31 14:54:19 +0300

    info | add plugin info

--- WpQuiz/career-orientation.php
+++ WpQuiz/career-orientation.php
@@ -1,9 +1,11 @@
 <?php
 /*
 Plugin Name: Career Orientation
+Plugin URI: https://github.com/hmaster20/WPquiz
 Description: A WordPress plugin for career orientation with weighted answers, categories, rubrics, analytics, reports, and one-time quiz links.
 Version: 1.6
 Author: Hmaster20
+Author URI: https://github.com/hmaster20
 License: GPL2
 Text Domain: career-orientation
 */
